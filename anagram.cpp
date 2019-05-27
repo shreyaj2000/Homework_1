4b2c810d073075d599b292e095f6e1a2277fd54e
shreya <shreya.sharad.jadhav@gmail.com>
2019-05-27 22:52:02 +0900

    incremental goal 4

--- anagram.cpp
+++ anagram.cpp
@@ -1,14 +1,16 @@
-//Adapt that program to find anagrams that use only a subset of characters
+//optimize to find the highest scoring anagram, rather than just the longest one.
 #include<iostream>
 #include<fstream>
 #include<algorithm>
 
 using namespace std;
 
-string dictionary,str,dictionary_sort,str_sort;
+string dictionary,str,dictionary_sort,str_sort,max_word,longest_word;
+int max_score,longest_len;
 
 void sort(string, string);
-void find_anagrams();
+bool find_anagrams();
+void calculate_score();
 
 int main() 
 {
@@ -28,22 +30,21 @@ int main()
 		exit(0);
 	}
 
-	//obtain word and length
-
-	cout<<"\nWords present in the anagram are :\n";
+	cout<<"\nAnagrams formed from string are :\n";
 
 	while(!file.eof()){
 		getline(file, dictionary);
 		sort(dictionary, str);
-		find_anagrams();
+		if (find_anagrams()) {
+			calculate_score();
+		}
 	}
 
-	cout<<"\n";
+	cout<<"\n\nLongest word formed is "<<longest_word<<" with a length of "<<longest_len<<"\n";
+	cout<<"\nHighest scoring anagram is "<<max_word<<" with a score of "<<max_score<<"\n";
 
 	return 0;
 }
-
-//sort letters
 
 void sort(string dictionary, string str)
 {
@@ -54,10 +55,10 @@ void sort(string dictionary, string str)
 
 }
 
-void find_anagrams()
+bool find_anagrams()
 {
-	int i,j, count;
-	j=count=0;
+	int i,j, count,len;
+	j=count=len=0;
 
 	for(i = 0; i<str.length(); i++) {
     	char c = str_sort[i];
@@ -65,12 +66,43 @@ void find_anagrams()
     	if (c==w) {
     		++count;
     		++j;
+    		++len;
     	}
 	}
 
-	if (count==dictionary_sort.length())
-		cout<<dictionary<<"\t";
 
+	if (count==dictionary_sort.length()) {
+		cout<<dictionary<<"\t";
+		if (longest_len<len) {
+			longest_len = len;
+			longest_word = dictionary;
+		}
+		return true;
+	}
+	return false;
+
+}
+
+void calculate_score() {
+
+	int score=0;
+	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	int points[26]={1,1,2,1,1,2,1,2,1,3,3,2,2,1,1,2,3,1,1,1,1,2,2,3,2,3};
+	int j=0;
+
+	for (int i=0; i<26; i++) {
+		char c = dictionary_sort[j];
+		if (alphabet[i]==c) {
+			score+= points[i];
+			j++;
+		}
+	}
+	score = score*score;
+
+	if (max_score<score) {
+		max_score = score;
+		max_word = dictionary;
+	}
 }
 
 
