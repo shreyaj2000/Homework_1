1e9c03f1cd3acf6d285ec4804a6dd999ee68445c
shreya <shreya.sharad.jadhav@gmail.com>
2019-05-29 10:40:51 +0900

    minor changes and optimizing code

--- anagram.cpp
+++ anagram.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-string dictionary,str,dictionary_sort,str_sort,max_word,longest_word;
-int max_score,longest_len;
+string dictionary_word,word,dictionary_word_sort,word_sort,max_word;
+int max_score;
 
-void sort(string, string);
-bool find_anagrams();
-void calculate_score();
+void sort(string, string); //sorts the dictionary word and the string entered
+bool find_anagrams(); //checks if the dictionary word is present in the string enetered
+void calculate_highest_score(); //calculates which dictionary word has the highest score
 
 int main() 
 {
@@ -18,10 +18,8 @@ int main()
 	//Reading file
 	
 	cout<<"\nEnter characters : ";
-	cin>>str;
-	int length_of_anagram = str.length();
-
-	int i=0;
+	cin>>word;
+	int length_of_anagram = word.length();
 
 	ifstream file("dictionary.txt", ios::in);
 	
@@ -30,28 +28,29 @@ int main()
 		exit(0);
 	}
 
+	//Checking if each word is anagram. Also find highest scoring word
+
 	cout<<"\nAnagrams formed from string are :\n";
 
 	while(!file.eof()){
-		getline(file, dictionary);
-		sort(dictionary, str);
+		getline(file, dictionary_word);
+		sort(dictionary_word, word);
 		if (find_anagrams()) {
-			calculate_score();
+			calculate_highest_score();
 		}
 	}
 
-	cout<<"\n\nLongest word formed is "<<longest_word<<" with a length of "<<longest_len<<"\n";
 	cout<<"\nHighest scoring anagram is "<<max_word<<" with a score of "<<max_score<<"\n";
 
 	return 0;
 }
 
-void sort(string dictionary, string str)
+void sort(string dictionary_word, string word)
 {
-	dictionary_sort = dictionary;
-	str_sort = str;
-	sort(dictionary_sort.begin(), dictionary_sort.end());
-	sort(str_sort.begin(), str_sort.end());
+	dictionary_word_sort = dictionary_word;
+	word_sort = word;
+	sort(dictionary_word_sort.begin(), dictionary_word_sort.end());
+	sort(word_sort.begin(), word_sort.end());
 
 }
 
@@ -60,9 +59,9 @@ bool find_anagrams()
 	int i,j, count,len;
 	j=count=len=0;
 
-	for(i = 0; i<str.length(); i++) {
-    	char c = str_sort[i];
-    	char w = dictionary_sort[j];
+	for(i = 0; i<word.length(); i++) {
+    	char c = word_sort[i];
+    	char w = dictionary_word_sort[j];
     	if (c==w) {
     		++count;
     		++j;
@@ -71,19 +70,15 @@ bool find_anagrams()
 	}
 
 
-	if (count==dictionary_sort.length()) {
-		cout<<dictionary<<"\t";
-		if (longest_len<len) {
-			longest_len = len;
-			longest_word = dictionary;
-		}
+	if (count==dictionary_word_sort.length()) {
+		cout<<dictionary_word<<"\t";
 		return true;
 	}
 	return false;
 
 }
 
-void calculate_score() {
+void calculate_highest_score() {
 
 	int score=0;
 	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
@@ -91,10 +86,12 @@ void calculate_score() {
 	int j=0;
 
 	for (int i=0; i<26; i++) {
-		char c = dictionary_sort[j];
+		char c = dictionary_word_sort[j];
 		if (alphabet[i]==c) {
 			score+= points[i];
 			j++;
+			if (c=='q')
+				score -=1; //Since Qu is considered as 3 points together, we subtract 1 for the additional u detected
 		}
 		else if (c==alphabet[i-1]) {
 			score+= points[i-1]; //for words with two of the same letters
@@ -105,11 +102,12 @@ void calculate_score() {
 			j++;
 		}
 	}
+
 	score = score*score;
 
 	if (max_score<score) {
 		max_score = score;
-		max_word = dictionary;
+		max_word = dictionary_word;
 	}
 }
 
